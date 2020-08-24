5f86e59100f8e7fdf2a93062cf057b96125cdcb1
Vavrines <tianbaixiao@gmail.com>
2020-08-24 16:42:07 +0200

    Add weno5

--- src/slope.jl
+++ src/slope.jl
@@ -4,12 +4,11 @@
 
 export vanleer, minmod, superbee, vanalbaba
 export reconstruct2, reconstruct2!, reconstruct3, reconstruct3!
+export weno5
 
-
-"""
-Slope limiter functions
-
-"""
+# ------------------------------------------------------------
+# Slope limiter functions
+# ------------------------------------------------------------
 
 vanleer(sL::Real, sR::Real) =
     (fortsign(1.0, sL) + fortsign(1.0, sR)) * abs(sL) * abs(sR) /
@@ -36,14 +35,14 @@ end
 vanalbaba(sL, sR) = (sL^2 * sR + sL * sR^2) / (sL^2 + sR^2 + 1.e-7)
 
 
-"""
-Reconstruction methodologies
+# ------------------------------------------------------------
+# Reconstruction methodologies
+# ------------------------------------------------------------
 
 """
+Two-cell reconstruction
 
-# ------------------------------------------------------------
-# Two-cell reconstruction
-# ------------------------------------------------------------
+"""
 reconstruct2(wL::Real, wR::Real, Δx::Real) = (wR - wL) / Δx
 
 
@@ -115,26 +114,27 @@ function reconstruct2!(
 end
 
 
-# ------------------------------------------------------------
-# Three-cell reconstruction
-# ------------------------------------------------------------
+"""
+Three-cell reconstruction
+
+"""
 function reconstruct3(
     wL::Real,
     wN::Real,
     wR::Real,
     ΔxL::Real,
     ΔxR::Real,
-    limiter = "vanleer"::AbstractString,
+    limiter = :vanleer::Symbol,
 )
 
     sL = (wN - wL) / ΔxL
     sR = (wR - wN) / ΔxR
 
-    if limiter == "linear"
+    if limiter == :linear
         return 0.5 * (sL + sR)
-    elseif limiter == "vanleer"
+    elseif limiter == :vanleer
         return vanleer(sL, sR)
-    elseif limiter == "minmod"
+    elseif limiter == :minmod
         return minmod(sL, sR)
     else
     end
@@ -147,17 +147,17 @@ function reconstruct3(
     wR::AbstractArray{<:Real,1},
     ΔxL::Real,
     ΔxR::Real,
-    limiter = "vanleer"::AbstractString,
+    limiter = :vanleer::Symbol,
 )
 
     sL = (wN .- wL) ./ ΔxL
     sR = (wR .- wN) ./ ΔxR
 
-    if limiter == "linear"
+    if limiter == :linear
         return 0.5 .* (sL .+ sR)
-    elseif limiter == "vanleer"
+    elseif limiter == :vanleer
         return vanleer.(sL, sR)
-    elseif limiter == "minmod"
+    elseif limiter == :minmod
         return minmod.(sL, sR)
     else
     end
@@ -172,17 +172,17 @@ function reconstruct3!(
     wR::AbstractArray{<:Real,1},
     ΔxL::Real,
     ΔxR::Real,
-    limiter = "vanleer"::AbstractString,
+    limiter = :vanleer::Symbol,
 )
 
     sL = (wN .- wL) ./ ΔxL
     sR = (wR .- wN) ./ ΔxR
 
-    if limiter == "linear"
+    if limiter == :linear
         sw .= 0.5 .* (sL .+ sR)
-    elseif limiter == "vanleer"
+    elseif limiter == :vanleer
         sw .= vanleer.(sL, sR)
-    elseif limiter == "minmod"
+    elseif limiter == :minmod
         sw .= minmod.(sL, sR)
     else
     end
@@ -196,7 +196,7 @@ function reconstruct3(
     wR::AbstractArray{<:Real,2},
     ΔxL::Real,
     ΔxR::Real,
-    limiter = "vanleer"::AbstractString,
+    limiter = :vanleer::Symbol,
 )
 
     s = zeros(axes(wL))
@@ -216,7 +216,7 @@ function reconstruct3!(
     wR::AbstractArray{<:Real,2},
     ΔxL::Real,
     ΔxR::Real,
-    limiter = "vanleer"::AbstractString,
+    limiter = :vanleer::Symbol,
 )
 
     for j in axes(sw, 2)
@@ -233,7 +233,7 @@ function reconstruct3(
     wR::AbstractArray{<:Real,3},
     ΔxL::Real,
     ΔxR::Real,
-    limiter = "vanleer"::AbstractString,
+    limiter = :vanleer::Symbol,
 )
 
     s = zeros(axes(wL))
@@ -253,12 +253,63 @@ function reconstruct3!(
     wR::AbstractArray{<:Real,3},
     ΔxL::Real,
     ΔxR::Real,
-    limiter = "vanleer"::AbstractString,
+    limiter = :vanleer::Symbol,
 )
 
     for k in axes(sw, 3), j in axes(sw, 2)
         swjk = @view sw[:, j, k]
         reconstruct3!(swjk, wL[:, j, k], wN[:, j, k], wR[:, j, k], ΔxL, ΔxR, limiter)
     end
+
+end
+
+
+function weno5(wL2::Real, wL1::Real, wN::Real, wR1::Real, wR2::Real)
+
+    ϵ = 1e-6
+
+    β0 = 13. / 12. * (wN-2.*wR1+wR2)^2+1./4.*(3.*wN-4.*wR1+wR2)^2
+    β1 = 13. / 12. *(wL1-2.*wN+wR1)^2+1./4.*(wL1-wR1)^2
+    β2 = 13. / 12. *(wL2-2.*wL1+wN)^2+1./4.*(wL2-4.*wL1+3.*wN)^2
+
+    #--- right interface ---#
+    dr0 = 0.3
+    dr1 = 0.6
+    dr2 = 0.1
+
+    αr0 = dr0/(ϵ+β0)^2
+    αr1 = dr1/(ϵ+β1)^2
+    αr2 = dr2/(ϵ+β2)^2
+
+    ωr0 = αr0/(αr0+αr1+αr2)
+    ωr1 = αr1/(αr0+αr1+αr2)
+    ωr2 = αr2/(αr0+αr1+αr2)
+
+    qr0 = 1./3.*wN+5./6.*wR1-1./6.*wR2
+    qr1 = -1./6.*wL1+5./6.*wN+1./3.*wR1
+    qr2 = 1./3.*wL2-7./6.*wL1+11./6.*wN
+
+    wR = ωr0*qr0+ωr1*qr1+ωr2*qr2
+
+    #--- left interface ---#
+    dl0 = 0.1
+    dl1 = 0.6
+    dl2 = 0.3
+
+    αl0 = dl0/(ϵ+β0)^2
+    αl1 = dl1/(ϵ+β1)^2
+    αl2 = dl2/(ϵ+β2)^2
+
+    ωl0 = αl0/(αl0+αl1+αl2)
+    ωl1 = αl1/(αl0+αl1+αl2)
+    ωl2 = αl2/(αl0+αl1+αl2)
+
+    ql0 = 11./6.*wN-7./6.*wR1+1./3.*wR2
+    ql1 = 1./3.*wL1+5./6.*wN-1./6.*wR1
+    ql2 = -1./6.*wL2+5./6.*wL1+1./3.*wN
+
+    wL = αl0*ql0+αl1*ql1+αl2*ql2
+
+    return wL, wR
 
 end
