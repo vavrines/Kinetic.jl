fea49c9600b33a58a8f4dca5b757304155ff7615
Vavrines <tianbaixiao@gmail.com>
2020-09-05 15:10:28 +0200

    Revise

--- src/Theory/theory.jl
+++ src/Theory/theory.jl
@@ -2,6 +2,16 @@
 # Theories
 # ============================================================
 
+export prim_conserve, 
+    conserve_prim, 
+    mixture_prim_conserve, 
+    mixture_conserve_prim, 
+    em_coefficients, 
+    advection_flux, 
+    burgers_flux, 
+    euler_flux, 
+    euler_jacobi
+
 export gauss_moments,
     mixture_gauss_moments,
     moments_conserve,
@@ -17,27 +27,19 @@ export gauss_moments,
     mixture_maxwellian,
     reduce_distribution,
     full_distribution,
-    conserve_prim,
-    mixture_conserve_prim,
-    prim_conserve,
-    mixture_prim_conserve,
-    heat_capacity_ratio,
     ref_vhs_vis,
-    sound_speed,
     vhs_collision_time,
     aap_hs_collision_time,
     aap_hs_prim,
     aap_hs_diffeq,
     shift_pdf!,
-    em_coefficients,
     hs_boltz_kn,
     kernel_mode,
     boltzmann_fft,
-    boltzmann_fft!,
-    advection_flux,
-    burgers_flux,
-    euler_flux,
-    euler_jacobi
+    boltzmann_fft!
+
+export heat_capacity_ratio, sound_speed
 
 include("continuum.jl")
 include("atom.jl")
+include("thermo.jl")
