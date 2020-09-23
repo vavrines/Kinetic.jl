4c9a854e0041393004031685bdd6bb82e3ac3185
Tianbai Xiao <tianbaixiao@gmail.com>
2020-09-23 09:44:44 +0200

    Sync

--- example/plasma_briowu_1d.jl
+++ example/plasma_briowu_1d.jl
@@ -7,16 +7,72 @@ dt = Kinetic.timestep(ks, ctr, simTime)
 nt = Int(floor(ks.set.maxTime / dt))+1
 res = zeros(5, 2)
 
+KS = ks
 @showprogress for iter in 1:nt
     #dt = timestep(KS, ctr, simTime)
-#    Kinetic.reconstruct!(ks, ctr)
-    Kinetic.evolve!(ks, ctr, face, dt; mode=:kfvs, isPlasma=true)
-    Kinetic.update!(ks, ctr, face, dt, res; coll=:bgk, bc=:extra, isMHD=true)
+    Kinetic.reconstruct!(ks, ctr)
+    #Kinetic.evolve!(ks, ctr, face, dt; mode=:kfvs, isPlasma=true)
+    @inbounds Threads.@threads for i = 1:KS.pSpace.nx+1
+        flux_kfvs!(
+            face[i].fw,
+            face[i].fh0,
+            face[i].fh1,
+            face[i].fh2,
+            face[i].fh3,
+            ctr[i-1].h0 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh0,
+            ctr[i-1].h1 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh1,
+            ctr[i-1].h2 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh2,
+            ctr[i-1].h3 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh3,
+            ctr[i].h0 .- 0.5 .* ctr[i].dx .* ctr[i].sh0,
+            ctr[i].h1 .- 0.5 .* ctr[i].dx .* ctr[i].sh1,
+            ctr[i].h2 .- 0.5 .* ctr[i].dx .* ctr[i].sh2,
+            ctr[i].h3 .- 0.5 .* ctr[i].dx .* ctr[i].sh3,
+            KS.vSpace.u,
+            KS.vSpace.weights,
+            dt,
+            ctr[i-1].sh0,
+            ctr[i-1].sh1,
+            ctr[i-1].sh2,
+            ctr[i-1].sh3,
+            ctr[i].sh0,
+            ctr[i].sh1,
+            ctr[i].sh2,
+            ctr[i].sh3,
+        )
+    end
+    @inbounds Threads.@threads for i = 1:KS.pSpace.nx+1
+        flux_em!(
+            face[i].femL,
+            face[i].femR,
+            ctr[i-2].E,
+            ctr[i-2].B,
+            ctr[i-1].E,
+            ctr[i-1].B,
+            ctr[i].E,
+            ctr[i].B,
+            ctr[i+1].E,
+            ctr[i+1].B,
+            ctr[i-1].ϕ,
+            ctr[i].ϕ,
+            ctr[i-1].ψ,
+            ctr[i].ψ,
+            ctr[i-1].dx,
+            ctr[i].dx,
+            KS.gas.Ap,
+            KS.gas.An,
+            KS.gas.D,
+            KS.gas.sol,
+            KS.gas.χ,
+            KS.gas.ν,
+            dt,
+        )
+    end
+    #Kinetic.update!(ks, ctr, face, dt, res; coll=:bgk, bc=:extra, isMHD=true)
     # it's equivalent to the following process
-    #=
+    
     sumRes = zeros(5, 2)
     sumAvg = zeros(5, 2)
-    Threads.@threads for i = 1:KS.pSpace.nx
+    Threads.@threads for i = 1:ks.pSpace.nx
         Kinetic.step!(
             ks,
             face[i],
@@ -27,7 +83,31 @@ res = zeros(5, 2)
             sumAvg,
         )
     end
-    =#
+    
+    for i = 1:2
+        ctr[1-i].w .= ctr[1].w
+        ctr[1-i].prim .= ctr[1].prim
+        ctr[1-i].h0 .= ctr[1].h0
+        ctr[1-i].h1 .= ctr[1].h1
+        ctr[1-i].h2 .= ctr[1].h2
+        ctr[1-i].E .= ctr[1].E
+        ctr[1-i].B .= ctr[1].B
+        ctr[1-i].ϕ = deepcopy(ctr[1].ϕ)
+        ctr[1-i].ψ = deepcopy(ctr[1].ψ)
+        ctr[1-i].lorenz .= ctr[1].lorenz
+
+        ctr[KS.pSpace.nx+i].w .= ctr[KS.pSpace.nx].w
+        ctr[KS.pSpace.nx+i].prim .= ctr[KS.pSpace.nx].prim
+        ctr[KS.pSpace.nx+i].h0 .= ctr[KS.pSpace.nx].h0
+        ctr[KS.pSpace.nx+i].h1 .= ctr[KS.pSpace.nx].h1
+        ctr[KS.pSpace.nx+i].h2 .= ctr[KS.pSpace.nx].h2
+        ctr[KS.pSpace.nx+i].E .= ctr[KS.pSpace.nx].E
+        ctr[KS.pSpace.nx+i].B .= ctr[KS.pSpace.nx].B
+        ctr[KS.pSpace.nx+i].ϕ = deepcopy(ctr[KS.pSpace.nx].ϕ)
+        ctr[KS.pSpace.nx+i].ψ = deepcopy(ctr[KS.pSpace.nx].ψ)
+        ctr[KS.pSpace.nx+i].lorenz .= ctr[KS.pSpace.nx].lorenz
+    end
+
 end
 
 sol = zeros(ks.pSpace.nx, 10, 2)
--- test/test_briowu.jl
+++ test/test_briowu.jl
@@ -1,5 +1,5 @@
-using Revise, Kinetic
-#=
+using Revise#, Kinetic
+
 cd(@__DIR__)
 begin
     using Dates
@@ -13,6 +13,7 @@ begin
     using JLD2
     using Plots
     using PyCall
+    using ProgressMeter
     include("../src/Data/data.jl")
     include("../src/IO/io.jl")
     include("../src/Math/math.jl")
@@ -24,7 +25,7 @@ begin
     include("../src/Config/config.jl")
     include("../src/Solver/solver.jl")
 end
-=#
+
 function flux_mhd!(
     fw::AbstractArray{<:AbstractFloat,2},
     fh0::AbstractArray{<:AbstractFloat,2},
@@ -127,52 +128,176 @@ function flux_mhd!(
         @. fh2[:, j] = Mt[1, j] * u[:, j] * g2[:, j] + Mt[2, j] * u[:, j] * h2[:, j]
         @. fh3[:, j] = Mt[1, j] * u[:, j] * g3[:, j] + Mt[2, j] * u[:, j] * h3[:, j]
     end
+end
 
+function step_mhd!(
+    KS::SolverSet,
+    faceL::Interface1D4F,
+    cell::ControlVolume1D4F,
+    faceR::Interface1D4F,
+    dt::AbstractFloat,
+)
+    #--- update conservative flow variables: step 1 ---#
+    # w^n
+    w_old = deepcopy(cell.w)
+    prim_old = deepcopy(cell.prim)
+
+    # flux -> w^{n+1}
+    @. cell.w += (faceL.fw - faceR.fw) / cell.dx
+    cell.prim .= mixture_conserve_prim(cell.w, KS.gas.γ)
+
+    #--- update electromagnetic variables ---#
+    # flux -> E^{n+1} & B^{n+1}
+    cell.E[1] -= dt * (faceL.femR[1] + faceR.femL[1]) / cell.dx
+    cell.E[2] -= dt * (faceL.femR[2] + faceR.femL[2]) / cell.dx
+    cell.E[3] -= dt * (faceL.femR[3] + faceR.femL[3]) / cell.dx
+    cell.B[1] -= dt * (faceL.femR[4] + faceR.femL[4]) / cell.dx
+    cell.B[2] -= dt * (faceL.femR[5] + faceR.femL[5]) / cell.dx
+    cell.B[3] -= dt * (faceL.femR[6] + faceR.femL[6]) / cell.dx
+    cell.ϕ -= dt * (faceL.femR[7] + faceR.femL[7]) / cell.dx
+    cell.ψ -= dt * (faceL.femR[8] + faceR.femL[8]) / cell.dx
+
+    for i = 1:3
+        if 1 ∈ vcat(isnan.(cell.E), isnan.(cell.B))
+            @warn "NaN electromagnetic update"
+        end
+    end
+
+    # source -> U^{n+1}, E^{n+1} and B^{n+1}
+    mr = KS.gas.mi / KS.gas.me
+    A, b =
+        em_coefficients(cell.prim, cell.E, cell.B, mr, KS.gas.lD, KS.gas.rL, dt)
+    x = A \ b
+
+    #--- calculate lorenz force ---#
+    cell.lorenz[1, 1] =
+        0.5 * (
+            x[1] + cell.E[1] + (cell.prim[3, 1] + x[5]) * cell.B[3] -
+            (cell.prim[4, 1] + x[6]) * cell.B[2]
+        ) / KS.gas.rL
+    cell.lorenz[2, 1] =
+        0.5 * (
+            x[2] + cell.E[2] + (cell.prim[4, 1] + x[6]) * cell.B[1] -
+            (cell.prim[2, 1] + x[4]) * cell.B[3]
+        ) / KS.gas.rL
+    cell.lorenz[3, 1] =
+        0.5 * (
+            x[3] + cell.E[3] + (cell.prim[2, 1] + x[4]) * cell.B[2] -
+            (cell.prim[3, 1] + x[5]) * cell.B[1]
+        ) / KS.gas.rL
+    cell.lorenz[1, 2] =
+        -0.5 *
+        (
+            x[1] + cell.E[1] + (cell.prim[3, 2] + x[8]) * cell.B[3] -
+            (cell.prim[4, 2] + x[9]) * cell.B[2]
+        ) *
+        mr / KS.gas.rL
+    cell.lorenz[2, 2] =
+        -0.5 *
+        (
+            x[2] + cell.E[2] + (cell.prim[4, 2] + x[9]) * cell.B[1] -
+            (cell.prim[2, 2] + x[7]) * cell.B[3]
+        ) *
+        mr / KS.gas.rL
+    cell.lorenz[3, 2] =
+        -0.5 *
+        (
+            x[3] + cell.E[3] + (cell.prim[2, 2] + x[7]) * cell.B[2] -
+            (cell.prim[3, 2] + x[8]) * cell.B[1]
+        ) *
+        mr / KS.gas.rL
+
+    cell.E[1] = x[1]
+    cell.E[2] = x[2]
+    cell.E[3] = x[3]
+
+    #--- update conservative flow variables: step 2 ---#
+    cell.prim[2, 1] = x[4]
+    cell.prim[3, 1] = x[5]
+    cell.prim[4, 1] = x[6]
+    cell.prim[2, 2] = x[7]
+    cell.prim[3, 2] = x[8]
+    cell.prim[4, 2] = x[9]
+
+    cell.w .= mixture_prim_conserve(cell.prim, KS.gas.γ)
+
+    #--- update particle distribution function ---#
+    # flux -> f^{n+1}
+    @. cell.h0 += (faceL.fh0 - faceR.fh0) / cell.dx
+    @. cell.h1 += (faceL.fh1 - faceR.fh1) / cell.dx
+    @. cell.h2 += (faceL.fh2 - faceR.fh2) / cell.dx
+    @. cell.h3 += (faceL.fh3 - faceR.fh3) / cell.dx
+
+    # force -> f^{n+1} : step 1
+    for j in axes(cell.h0, 2)
+        _h0 = @view cell.h0[:, j]
+        _h1 = @view cell.h1[:, j]
+        _h2 = @view cell.h2[:, j]
+        _h3 = @view cell.h3[:, j]
+
+        shift_pdf!(_h0, cell.lorenz[1, j], KS.vSpace.du[1, j], dt)
+        shift_pdf!(_h1, cell.lorenz[1, j], KS.vSpace.du[1, j], dt)
+        shift_pdf!(_h2, cell.lorenz[1, j], KS.vSpace.du[1, j], dt)
+        shift_pdf!(_h3, cell.lorenz[1, j], KS.vSpace.du[1, j], dt)
+    end
+
+    # force -> f^{n+1} : step 2
+    for k in axes(cell.h1, 3)
+        @. cell.h3[:, k] +=
+            2.0 * dt * cell.lorenz[2, k] * cell.h1[:, k] +
+            (dt * cell.lorenz[2, k])^2 * cell.h0[:, k] +
+            2.0 * dt * cell.lorenz[3, k] * cell.h2[:, k] +
+            (dt * cell.lorenz[3, k])^2 * cell.h0[:, k]
+        @. cell.h2[:, k] += dt * cell.lorenz[3, k] * cell.h0[:, k]
+        @. cell.h1[:, k] += dt * cell.lorenz[2, k] * cell.h0[:, k]
+    end
+
+    # source -> f^{n+1}
+    tau = aap_hs_collision_time(
+        cell.prim,
+        KS.gas.mi,
+        KS.gas.ni,
+        KS.gas.me,
+        KS.gas.ne,
+        KS.gas.Kn[1],
+    )
+
+    # interspecies interaction
+    prim = aap_hs_prim(cell.prim, tau, KS.gas.mi, KS.gas.ni, KS.gas.me, KS.gas.ne, KS.gas.Kn[1])
+    g = mixture_maxwellian(KS.vSpace.u, prim)
+
+    # BGK term
+    Mu, Mv, Mw, MuL, MuR = mixture_gauss_moments(prim, KS.gas.K)
+    for k in axes(cell.h0, 2)
+        @. cell.h0[:, k] =
+            (cell.h0[:, k] + dt / tau[k] * g[:, k]) / (1.0 + dt / tau[k])
+        @. cell.h1[:, k] =
+            (cell.h1[:, k] + dt / tau[k] * Mv[1, k] * g[:, k]) /
+            (1.0 + dt / tau[k])
+        @. cell.h2[:, k] =
+            (cell.h2[:, k] + dt / tau[k] * Mw[1, k] * g[:, k]) /
+            (1.0 + dt / tau[k])
+        @. cell.h3[:, k] =
+            (cell.h3[:, k] + dt / tau[k] * (Mv[2, k] + Mw[2, k]) * g[:, k]) /
+            (1.0 + dt / tau[k])
+    end
 end
 
 cd(@__DIR__)
-ks, ctr, face, simTime = Kinetic.initialize("briowu.txt")
+ks, ctr, face, simTime = initialize("briowu.txt")
 KS = ks
 
-dt = Kinetic.timestep(ks, ctr, simTime)
+dt = timestep(ks, ctr, simTime)
 nt = Int(floor(ks.set.maxTime / dt))+1
 res = zeros(5, 2)
 
-for iter in 1:nt
+@showprogress for iter in 1:nt
     #dt = timestep(KS, ctr, simTime)
-    Kinetic.reconstruct!(ks, ctr)
-    Kinetic.evolve!(ks, ctr, face, dt; mode=:kfvs, isPlasma=true)
-    #=    
+    reconstruct!(ks, ctr)
+
+    #evolve!(ks, ctr, face, dt; mode=:kfvs, isPlasma=true)
     @inbounds Threads.@threads for i = 1:KS.pSpace.nx+1
         #=
-        flux_tst!(
-            face[i].fw,
-            face[i].fh0,
-            face[i].fh1,
-            face[i].fh2,
-            face[i].fh3,
-            ctr[i-1].w .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sw,
-            ctr[i-1].h0 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh0,
-            ctr[i-1].h1 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh1,
-            ctr[i-1].h2 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh2,
-            ctr[i-1].h3 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh3,
-            ctr[i].w .- 0.5 .* ctr[i].dx .* ctr[i].sw,
-            ctr[i].h0 .- 0.5 .* ctr[i].dx .* ctr[i].sh0,
-            ctr[i].h1 .- 0.5 .* ctr[i].dx .* ctr[i].sh1,
-            ctr[i].h2 .- 0.5 .* ctr[i].dx .* ctr[i].sh2,
-            ctr[i].h3 .- 0.5 .* ctr[i].dx .* ctr[i].sh3,
-            KS.vSpace.u,
-            KS.vSpace.weights,
-            ks.gas.K,
-            ks.gas.γ,
-            ks.gas.mi,
-            ks.gas.ni,
-            ks.gas.me,
-            ks.gas.ne,
-            ks.gas.Kn[1],
-            dt,
-        )
-        =#
         flux_kfvs!(
             face[i].fw,
             face[i].fh0,
@@ -198,6 +323,34 @@ for iter in 1:nt
             ctr[i].sh1,
             ctr[i].sh2,
             ctr[i].sh3,
+        )
+        =#
+        flux_mhd!(
+            face[i].fw,
+            face[i].fh0,
+            face[i].fh1,
+            face[i].fh2,
+            face[i].fh3,
+            ctr[i-1].w .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sw,
+            ctr[i-1].h0 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh0,
+            ctr[i-1].h1 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh1,
+            ctr[i-1].h2 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh2,
+            ctr[i-1].h3 .+ 0.5 .* ctr[i-1].dx .* ctr[i-1].sh3,
+            ctr[i].w .- 0.5 .* ctr[i].dx .* ctr[i].sw,
+            ctr[i].h0 .- 0.5 .* ctr[i].dx .* ctr[i].sh0,
+            ctr[i].h1 .- 0.5 .* ctr[i].dx .* ctr[i].sh1,
+            ctr[i].h2 .- 0.5 .* ctr[i].dx .* ctr[i].sh2,
+            ctr[i].h3 .- 0.5 .* ctr[i].dx .* ctr[i].sh3,
+            KS.vSpace.u,
+            KS.vSpace.weights,
+            KS.gas.K,
+            KS.gas.γ,
+            KS.gas.mi,
+            KS.gas.ni,
+            KS.gas.me,
+            KS.gas.ne,
+            KS.gas.Kn[1],
+            dt,
         )
     end
     @inbounds Threads.@threads for i = 1:KS.pSpace.nx+1
@@ -227,10 +380,45 @@ for iter in 1:nt
             dt,
         )
     end
-    =#
-    Kinetic.update!(ks, ctr, face, dt, res; coll=:bgk, bc=:extra, isMHD=true)
+    
+    #update!(ks, ctr, face, dt, res; coll=:bgk, bc=:extra, isMHD=true)
+    @inbounds Threads.@threads for i in 1:KS.pSpace.nx
+        step_mhd!(
+            KS,
+            face[i],
+            ctr[i],
+            face[i+1],
+            dt,
+        )
+    end
+    for i in 1:2
+        ctr[1-i].w .= ctr[1].w
+        ctr[1-i].prim .= ctr[1].prim
+        ctr[KS.pSpace.nx+i].w .= ctr[KS.pSpace.nx].w
+        ctr[KS.pSpace.nx+i].prim .= ctr[KS.pSpace.nx].prim
+
+        ctr[1-i].h0 .= ctr[1].h0
+        ctr[1-i].h1 .= ctr[1].h1
+        ctr[1-i].h2 .= ctr[1].h2
+        ctr[1-i].h3 .= ctr[1].h3
+        ctr[1-i].E .= ctr[1].E
+        ctr[1-i].B .= ctr[1].B
+        ctr[1-i].ϕ = deepcopy(ctr[1].ϕ)
+        ctr[1-i].ψ = deepcopy(ctr[1].ψ)
+        ctr[1-i].lorenz .= ctr[1].lorenz
+
+        ctr[KS.pSpace.nx+i].h0 .= ctr[KS.pSpace.nx].h0
+        ctr[KS.pSpace.nx+i].h1 .= ctr[KS.pSpace.nx].h1
+        ctr[KS.pSpace.nx+i].h2 .= ctr[KS.pSpace.nx].h2
+        ctr[KS.pSpace.nx+i].h3 .= ctr[KS.pSpace.nx].h3
+        ctr[KS.pSpace.nx+i].E .= ctr[KS.pSpace.nx].E
+        ctr[KS.pSpace.nx+i].B .= ctr[KS.pSpace.nx].B
+        ctr[KS.pSpace.nx+i].ϕ = deepcopy(ctr[KS.pSpace.nx].ϕ)
+        ctr[KS.pSpace.nx+i].ψ = deepcopy(ctr[KS.pSpace.nx].ψ)
+        ctr[KS.pSpace.nx+i].lorenz .= ctr[KS.pSpace.nx].lorenz
+    end
 end
-#=
+
 sol = zeros(ks.pSpace.nx, 10, 2)
 for i in 1:ks.pSpace.nx
     sol[i, 1, 1] = ctr[i].prim[1,1]
@@ -245,6 +433,7 @@ for i in 1:ks.pSpace.nx
 end
 
 using Plots
-plot(ks.pSpace.x[1:ks.pSpace.nx], sol[:,1,:])
+plot(ks.pSpace.x[1:ks.pSpace.nx], sol[:,2,:])
 plot(ks.pSpace.x[1:ks.pSpace.nx], sol[:,6,:])
-=#
+
+savefig("kfvs.png")
