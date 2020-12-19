7014fa4f66e38646738f8379303e91217ceb91e7
Tianbai Xiao <tianbaixiao@gmail.com>
2020-12-19 09:55:55 +0100

    Update Kinetic.jl

--- src/Kinetic.jl
+++ src/Kinetic.jl
@@ -1,10 +1,20 @@
-# ============================================================
-# Kinetic.jl : A lightweight Julialang toolbox for kinetic 
-#              theory and scientific machine learning
-# Copyright (c) 2020 Tianbai Xiao <tianbaixiao@gmail.com>
-# ============================================================
+"""
+Kinetic.jl: A Lightweight Framework for Kinetic Theory and Scientific Machine Learning
 
+Copyright (c) 2020 Tianbai Xiao <<tianbaixiao@gmail.com>>
+"""
 module Kinetic
+
+export 動
+
+"""
+Lightweight Framework for Kinetic Theory and Scientific Machine Learning
+
+轻量化的动理论建模和计算框架
+
+"動" means "motion" in Chinese
+"""
+const 動 = Kinetic
 
 using Reexport
 @reexport using KitBase
