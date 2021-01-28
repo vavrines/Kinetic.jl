7caca62d3cbeccc132c8f8a223de2c22c7cb56cd
Tianbai Xiao <tianbaixiao@gmail.com>
2021-01-28 22:39:52 +0100

    Update version

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,15 @@
 using PyCall
 
-# try importing meshio
-# if not existed, install it
+# try importing meshio & catch the installation
+## 1) Julia built-in miniconda
+## 2) global pip installer
 try
-    meshio = pyimport("meshio")
-catch
     using Conda
     Conda.add_channel("conda-forge")
     Conda.add("meshio")
+    meshio = pyimport("meshio")
+catch
+    cmd = `pip3 install meshio --user`
+    run(cmd)
     meshio = pyimport("meshio")
 end
--- src/Kinetic.jl
+++ src/Kinetic.jl
@@ -1,18 +1,14 @@
 """
-Kinetic.jl: A Lightweight Framework for Computational Fluid Dynamics and Scientific Machine Learning
+Kinetic.jl: A Portable Framework for Computational Fluid Dynamics and Scientific Machine Learning
 
 Copyright (c) 2021 Tianbai Xiao (tianbaixiao@gmail.com)
 """
 module Kinetic
 
-if VERSION < v"1.3"
-    @warn "Kinetic.jl matches perfectly with Julia 1.3 and newer versions."
-end
-
 export 転
 
 """
-Lightweight Framework for Computational Fluid Dynamics and Scientific Machine Learning
+Portable Framework for Computational Fluid Dynamics and Scientific Machine Learning
 
 轻量化的计算流体力学建模和计算框架
 
