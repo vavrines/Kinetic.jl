f36b41918fab665fff2af81adaac41013ac3bfd9
vavrines <tianbaixiao@gmail.com>
2023-06-09 11:59:24 +0800

    Update benchmark

--- benchmark/cpu-vs-gpu.jl
+++ benchmark/cpu-vs-gpu.jl
@@ -1,1 +1,25 @@
+using KitBase, CUDA
 
+u = collect(-5.0:0.0001:5.0)
+primL = [1.0, 0.0, 0.5]
+primR = [0.125, 0.0, 0.625]
+dt = 1.0
+
+fL = maxwellian(u, primL)
+fR = maxwellian(u, primR)
+ff = similar(fL)
+sfL = zero(fL)
+sfR = zero(fR)
+
+@time flux_kfvs!(ff, fL, fR, u, dt, sfL, sfR)
+```0.001932 seconds (6 allocations: 2.289 MiB)```
+
+u = collect(-5.0:0.0001:5.0) |> CuArray
+fL = maxwellian(u, primL)
+fR = maxwellian(u, primR)
+ff = similar(fL)
+sfL = zero(fL)
+sfR = zero(fR)
+
+@time flux_kfvs!(ff, fL, fR, u, dt, sfL, sfR)
+```0.000219 seconds (203 allocations: 15.125 KiB)```
